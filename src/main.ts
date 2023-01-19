1ab0c430a70d01dc1f448c062b0641a7f7d7ef79
IGsayonara <50332314+IGsayonara@users.noreply.github.com>
2023-01-20 01:56:18 +0200

    chore: add cors protection (#3)
    Co-authored-by: IDIDUNI <ihor.didunik@ext.mercedes-benz.io>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:8080',
+      'http://luckyigor.world',
+      'http://dev.luckyigor.world',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
