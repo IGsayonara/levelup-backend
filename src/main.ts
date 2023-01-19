22847adc75f6cc83ad81fca76b4a710056f68463
IGsayonara <50332314+IGsayonara@users.noreply.github.com>
2023-01-19 21:13:55 +0200

    chore: enable all cors (#2)
    Co-authored-by: IDIDUNI <ihor.didunik@ext.mercedes-benz.io>

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:8080',
-      'http://luckyigor.world',
-      'http://dev.luckyigor.world',
-    ],
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
