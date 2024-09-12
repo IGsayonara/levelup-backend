059179bd25edc774d48fec92db582d0aab8575a4
noffi <diduniki@gmail.com>
2024-09-12 23:35:25 +0100

    feat: add project seeder

--- src/database/seeders/seeder.module.ts
+++ src/database/seeders/seeder.module.ts
@@ -2,9 +2,14 @@ import { Seeder } from './seeder';
 import { UserSeederModule } from './user.module';
 import { PostgrtesDatabaseProviderModule } from '../../providers/database/postgres/postgrtesDatabase.provider.module';
 import { Module } from '@nestjs/common';
+import { ProjectSeederModule } from './project.module';
 
 @Module({
-  imports: [PostgrtesDatabaseProviderModule, UserSeederModule],
+  imports: [
+    PostgrtesDatabaseProviderModule,
+    UserSeederModule,
+    ProjectSeederModule,
+  ],
   providers: [Seeder],
 })
 export class SeederModule {}
