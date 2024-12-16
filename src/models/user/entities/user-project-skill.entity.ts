2d531ee7baa8047197d7d514a55bdf3ccf99956a
noffi <diduniki@gmail.com>
2024-12-16 22:19:16 +0200

    feat: addUserProjectSkill

--- src/models/user/entities/user-project-skill.entity.ts
+++ src/models/user/entities/user-project-skill.entity.ts
@@ -4,11 +4,13 @@ import {
   Entity,
   ManyToOne,
   PrimaryGeneratedColumn,
+  Unique,
 } from 'typeorm';
 import { UserProjectEntity } from './user-project.entity';
 import { SkillEntity } from '../../skill/entities/skill.entity';
 
 @Entity('user_project_skill')
+@Unique(['userProject', 'skill'])
 export class UserProjectSkillEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
